432514c5d55cc8d173f9cf6dd544a8899923c983
Katharine Childs <katharine@raspberrypi.org>
2023-08-16 17:08:57 +0100

    Update wordle_tracker.py
    Added update to sort wordlehistory.txt alphabetically after every new Wordle has been added

--- wordle_tracker.py
+++ wordle_tracker.py
@@ -1,11 +1,20 @@
 def append_wordle_to_file(wordle_input, filename):
     with open(filename, 'a') as file:
         file.write(wordle_input + '\n')
+
+def sort_wordle_history(filename):
+    lines = []
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    lines.sort()
+    with open(filename, 'w') as file:
+        file.writelines(lines)
         
 def main():
     wordle_input = input("Enter the Wordle of the day: ")
     if wordle_input:
         append_wordle_to_file(wordle_input, 'wordlehistory.txt')
+        sort_wordle_history('wordlehistory.txt')
         print("Wordle successfully added to the history!")
 
 if __name__ == "__main__":
